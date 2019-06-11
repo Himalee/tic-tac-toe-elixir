04b0a9410a4bda50fe9e274331f0cb384f067909
Himalee <himalee.tailor@gmail.com>
2019-06-11 16:22:49 +0100

    Refactor: format() for better readability

--- lib/board_formatter.ex
+++ lib/board_formatter.ex
@@ -1,20 +1,30 @@
 defmodule BoardFormatter do
   def format(grid) do
     grid
+    |> format_with_index
+    |> create_rows
+    |> convert_to_presentable_format
+  end
+
+  defp format_with_index(grid) do
+    grid
     |> Enum.with_index
     |> Enum.map(&cell/1)
-    |> split_grid
-    |> Enum.intersperse("\n")
-    |> List.flatten
-    |> Enum.reduce("", fn str, acc -> acc <> str end)
   end
 
   defp cell(tuple) when elem(tuple, 1) > 9, do: "  #{elem(tuple, 0)} "
   defp cell(tuple) when true, do: "  #{elem(tuple, 0)}  "
 
-  defp split_grid(grid) do
+  defp create_rows(grid) do
     grid
     |> Enum.chunk_every(size(grid))
+    |> Enum.intersperse("\n")
+    |> List.flatten
+  end
+
+  defp convert_to_presentable_format(grid) do
+    grid
+    |> Enum.reduce("", fn str, acc -> acc <> str end)
   end
 
   defp size(grid) do
