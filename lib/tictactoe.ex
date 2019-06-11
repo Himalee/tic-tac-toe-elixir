f40b670d6ceded26a124f95f170bee56a8a2a6bf
Himalee <himalee.tailor@gmail.com>
2019-06-11 15:28:26 +0100

    Add: Validator

--- lib/tictactoe.ex
+++ lib/tictactoe.ex
@@ -6,12 +6,21 @@ defmodule TicTacToe do
     |> display_board
   end
 
-  def welcome_players do
+  defp welcome_players do
     Console.present(Message.welcome)
   end
 
   defp chosen_size_of_board do
     Console.receive(Message.size_of_board)
+    |> valid_board_size
+  end
+
+  defp valid_board_size(size) do
+    if Validator.is_valid_board_size?(size) do
+      size
+    else
+      chosen_size_of_board()
+    end
   end
 
   defp convert_size_of_board(user_choice) do
--- test/tictactoe_test.exs
+++ test/tictactoe_test.exs
@@ -13,7 +13,7 @@ defmodule TicTacToeTest do
     assert capture_io("3\n", context[:start]) =~ "Welcome"
   end
 
-  test "given player board size choice, display 3x3 board", context do
-    assert capture_io("3\n", context[:start]) =~ "0    1    2  \n  3    4    5  \n  6    7    8  \n"
+  test "given valid player board size choice, display 3x3 board", context do
+    assert capture_io("1\n3\n", context[:start]) =~ "0    1    2  \n  3    4    5  \n  6    7    8  \n"
   end
 end
