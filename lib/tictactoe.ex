08801ee013935032e3dc163169be108ef217b8aa
Himalee <himalee.tailor@gmail.com>
2019-06-11 10:30:30 +0100

    Add: player can choose size of board

--- lib/tictactoe.ex
+++ lib/tictactoe.ex
@@ -1,5 +1,30 @@
 defmodule TicTacToe do
   def start do
+    welcome_players()
+    chosen_size_of_board()
+    |> convert_size_of_board
+    |> display_board
+  end
+
+  def welcome_players do
     IO.puts("Welcome to Tic Tac Toe")
+  end
+
+  defp chosen_size_of_board do
+    IO.gets"Choose a board size, 3 for 3x3 or 4 for 4x4: "
+  end
+
+  defp convert_size_of_board(user_choice) do
+    user_choice
+    |> String.trim
+    |> Integer.parse
+    |> elem(0)
+  end
+
+  defp display_board(size) do
+    size
+    |> Board.new
+    |> Display.board
+    |> IO.puts
   end
 end
--- test/tictactoe_test.exs
+++ test/tictactoe_test.exs
@@ -1,8 +1,19 @@
 defmodule TicTacToeTest do
   use ExUnit.Case
+  import ExUnit.CaptureIO
   doctest TicTacToe
 
   test "greets players" do
-    assert TicTacToe.start() == :ok
+    execute_main = fn ->
+      TicTacToe.start()
+    end
+    assert capture_io("3\n", execute_main) =~ "Welcome"
+  end
+
+  test "given user choice, display 3x3 board" do
+    execute_main = fn ->
+      TicTacToe.start()
+    end
+    assert capture_io("3\n", execute_main) =~ "0    1    2  \n  3    4    5  \n  6    7    8  \n"
   end
 end
