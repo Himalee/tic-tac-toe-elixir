94046cdcb03bb54cbc440ac97d6629ecb877ba21
Himalee <himalee.tailor@gmail.com>
2019-06-13 14:05:09 +0100

    Add: ability to mark board until board is full

--- test/cli_test.exs
+++ test/cli_test.exs
@@ -11,7 +11,13 @@ defmodule CLITest do
 
  test "presents board" do
     assert capture_io([input: "1\n3\n", capture_prompt: false], fn ->
-      IO.write CLI.present_board
+      IO.write CLI.present_board([0, 1, 2, 3, 4, 5, 6, 7, 8])
     end) == "  0    1    2  \n  3    4    5  \n  6    7    8  \nok"
+  end
+
+ test "marks board with player choice '1'" do
+    assert capture_io([input: "1\n", capture_prompt: false], fn ->
+      IO.write CLI.present_board(CLI.mark_board([0, 1, 2, 3, 4, 5, 6, 7, 8], "X", "X", "O"))
+    end) == "  0    X    2  \n  3    4    5  \n  6    7    8  \nok"
   end
 end
--- test/tictactoe_test.exs
+++ test/tictactoe_test.exs
@@ -3,17 +3,19 @@ defmodule TicTacToeTest do
   import ExUnit.CaptureIO
   doctest TicTacToe
 
-  setup :start
-
-  defp start(_context) do
-    [start: fn -> TicTacToe.start() end]
+  setup(_context) do
+    {:ok, [start: fn -> TicTacToe.start() end]}
   end
 
   test "greets players", context do
-    assert capture_io("3\n", context[:start]) =~ "Welcome"
+    assert capture_io("3\n0\n1\n2\n3\n4\n5\n6\n7\n8\n", context[:start]) =~ "Welcome"
   end
 
   test "given valid player board size choice, display 3x3 board", context do
-    assert capture_io("1\n3\n", context[:start]) =~ "0    1    2  \n  3    4    5  \n  6    7    8  \n"
+    assert capture_io("3\n0\n1\n2\n3\n4\n5\n6\n7\n8\n", context[:start]) =~ "0    1    2  \n  3    4    5  \n  6    7    8  \n"
+  end
+
+  test "marks board until full", context do
+    assert capture_io("3\n0\n1\n2\n3\n4\n5\n6\n7\n8\n", context[:start]) =~ "X    O    X  \n  O    X    O  \n  X    O    X  \n"
   end
 end
