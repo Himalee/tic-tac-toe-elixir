181544d05a6d372ff368eb3b37ff39400c47587a
Himalee <himalee.tailor@gmail.com>
2019-06-13 14:04:14 +0100

    Update: new messages

--- lib/message.ex
+++ lib/message.ex
@@ -6,4 +6,12 @@ defmodule Message do
   def size_of_board do
     "Choose a board size, 3 for 3x3 or 4 for 4x4: "
   end
+
+  def move do
+    "Choose a move: "
+  end
+
+  def game_over do
+    "Game over!"
+  end
 end
