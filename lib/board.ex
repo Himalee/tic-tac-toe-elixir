f1a552c480708523cd1f6bc5f43711b8bbbcfca6
Himalee <himalee.tailor@gmail.com>
2019-06-13 10:16:31 +0100

    Add: Board.mark()

--- lib/board.ex
+++ lib/board.ex
@@ -6,6 +6,15 @@ defmodule Board do
     |> convert
   end
 
+  def mark(index, symbol, grid) do
+    List.replace_at(grid, index, symbol)
+  end
+
+  def available_moves(grid, mark_one, mark_two) do
+    grid
+    |> Enum.reject(fn x -> x == mark_one or x == mark_two end)
+  end
+
   defp convert(number) do
     Enum.to_list 0..number
   end
--- test/board_test.exs
+++ test/board_test.exs
@@ -9,4 +9,16 @@ defmodule BoardTest do
   test "creates empty 4x4 board" do
     assert length(Board.new(4)) == 16
   end
+
+  test "marks board using index 0" do
+    assert Board.mark(0, "X", Board.new(3)) == ["X", 1, 2, 3, 4, 5, 6, 7, 8]
+  end
+
+  test "marks board using index 1" do
+    assert Board.mark(1, "X", Board.new(3)) == [0, "X", 2, 3, 4, 5, 6, 7, 8]
+  end
+
+  test "checks for available moves" do
+    assert Board.available_moves([0, "X", 2, 3, "O", "O", "X", "X", 8], "X", "O") == [0, 2, 3, 8]
+  end
 end
