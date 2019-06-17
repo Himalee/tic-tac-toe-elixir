3df752a38c86c40d7e5600c19774be1bfd6c563e
Himalee <himalee.tailor@gmail.com>
2019-06-17 10:38:50 +0100

    Add: all possible winning lines

--- lib/board.ex
+++ lib/board.ex
@@ -18,6 +18,42 @@ defmodule Board do
     length(Enum.uniq(grid)) == 2
   end
 
+  def all_winning_lines(grid) do
+    rows(grid) ++ columns(grid) ++ diagonals(grid)
+  end
+
+  def rows(grid) do
+    grid
+    |> Enum.chunk_every(size(grid))
+  end
+
+  def columns(grid) do
+    grid
+    |> rows
+    |> List.zip
+    |> Enum.map(&Tuple.to_list/1)
+  end
+
+  def diagonals(grid) do
+    [diagonal_line([], grid, 0, size(grid) + 1), diagonal_line([], grid, size(grid) - 1, size(grid) - 1)]
+  end
+
+  def diagonal_line(line, grid, current_index, increase_index_by) do
+    if length(line) == size(grid) do
+      line
+    else
+      line ++ [Enum.at(grid, current_index)]
+      |> diagonal_line(grid, current_index + increase_index_by, increase_index_by)
+    end
+  end
+
+  def size(grid) do
+    grid
+    |> length
+    |> :math.sqrt
+    |> round
+  end
+
   defp convert(number) do
     Enum.to_list 0..number
   end
--- test/board_test.exs
+++ test/board_test.exs
@@ -26,4 +26,28 @@ defmodule BoardTest do
     assert Board.is_full?([0, "X", 2, 3, "O", "O", "X", "X", 8]) == false
     assert Board.is_full?(["O", "X", "X", "O", "O", "O", "X", "X", "X"]) == true
   end
+
+  test "gets rows from a 3x3 board" do
+    assert Board.rows([0, 1, 2, "X", 4, 5, "O", 7, 8]) == [[0, 1, 2], ["X", 4, 5], ["O", 7, 8]]
+  end
+
+  test "gets rows from a 4x4 board" do
+    assert Board.rows([0, 1, 2, "X", 4, 5, "O", 7, 8, 9, 10, 11, 12, 13, 14, 15]) == [[0, 1, 2, "X"], [4, 5, "O", 7], [8, 9, 10, 11], [12, 13, 14, 15]]
+  end
+
+  test "gets columns from 3x3 board" do
+    assert Board.columns([0, 1, 2, "X", 4, 5, "O", 7, 8]) == [[0, "X", "O"], [1, 4, 7], [2, 5, 8]]
+  end
+
+  test "gets columns from 4x4 board" do
+    assert Board.columns([0, 1, 2, "X", 4, 5, "O", 7, 8, 9, 10, 11, 12, 13, 14, 15]) == [[0, 4, 8, 12], [1, 5, 9, 13], [2, "O", 10, 14], ["X", 7, 11, 15]]
+  end
+
+  test "gets diagonals from a 3x3 board" do
+    assert Board.diagonals([0, 1, 2, "X", 4, 5, "O", 7, 8]) == [[0, 4, 8], [2, 4, "O"]]
+  end
+
+  test "gets diagonals from 4x4 board" do
+    assert Board.diagonals([0, 1, 2, "X", 4, 5, "O", 7, 8, 9, 10, 11, 12, 13, 14, 15]) ==  [[0, 5, 10, 15], ["X", "O", 9, 12]]
+  end
 end
