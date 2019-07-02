c1a6b92567c4ee1e8dccda10a1c50a4572225b14
Himalee <himalee.tailor@gmail.com>
2019-07-02 17:17:47 +0100

    Refactor: use pipe operator

--- lib/unbeatable_computer_player.ex
+++ lib/unbeatable_computer_player.ex
@@ -8,18 +8,25 @@ defimpl Player, for: UnbeatableComputerPlayer do
   end
 
   def get_move(unbeatable_computer_player, grid, mark_one, mark_two, _length_of_pause_after_move) do
-    current_mark = Player.get_mark(unbeatable_computer_player)
-    list_of_scores(grid, current_mark, mark_one, mark_two, 0)
+    unbeatable_computer_player
+    |> Player.get_mark
+    |> get_best_move(grid, mark_one, mark_two, 0)
   end
 
-  defp list_of_scores(grid, current_mark, mark_one, mark_two, depth) do
-    available_moves = Board.available_moves(grid, mark_one, mark_two)
-    scores = Enum.map(available_moves, fn move ->
-      move
-      |> Board.mark(current_mark, grid)
-      |> get_score(current_mark, mark_one, mark_two, depth)
+  defp get_best_move(current_mark, grid, mark_one, mark_two, depth) do
+    grid
+    |> available_moves(mark_one, mark_two)
+    |> Enum.map(fn move ->
+        move
+        |> Board.mark(current_mark, grid)
+        |> get_score(current_mark, mark_one, mark_two, depth)
     end)
-    pick_best_move_or_highest_score(depth, scores, available_moves)
+    |> pick_best_move_or_highest_score(depth, available_moves(grid, mark_one, mark_two))
+  end
+
+  defp available_moves(grid, mark_one, mark_two) do
+    grid
+    |> Board.available_moves(mark_one, mark_two)
   end
 
   defp get_score(grid, current_mark, mark_one, mark_two, depth) do
@@ -27,13 +34,13 @@ defimpl Player, for: UnbeatableComputerPlayer do
       if is_there_a_terminal_state?(grid) do
         score(grid)
       else
-        next_mark = switch_marks(current_mark, mark_one, mark_two)
-        list_of_scores(grid, next_mark, mark_one, mark_two, depth + 1)
+        switch_marks(current_mark, mark_one, mark_two)
+        |> get_best_move(grid, mark_one, mark_two, depth + 1)
       end
     score * -1
   end
 
-  defp pick_best_move_or_highest_score(depth, scores, available_moves) do
+  defp pick_best_move_or_highest_score(scores, depth, available_moves) do
     if depth == 0 do
       max_score = Enum.max(scores)
       index_of_best_score = Enum.find_index(scores, fn x -> x == max_score end)
