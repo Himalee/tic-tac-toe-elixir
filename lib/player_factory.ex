7816afbb18957f5dc243f2c98b62e08149a34216
Himalee <himalee.tailor@gmail.com>
2019-07-02 16:31:51 +0100

    Refactor: set_players/1

--- lib/player_factory.ex
+++ lib/player_factory.ex
@@ -1,5 +1,9 @@
 defmodule PlayerFactory do
-  def set_players(game_mode) when game_mode == 1, do: [%HumanPlayer{mark: "X"}, %HumanPlayer{mark: "O"}]
-  def set_players(game_mode) when game_mode == 2, do: [%HumanPlayer{mark: "X"}, %RandomComputerPlayer{mark: "O"}]
-  def set_players(game_mode) when game_mode == 3, do: [%HumanPlayer{mark: "X"}, %UnbeatableComputerPlayer{mark: "O"}]
+  def set_players(game_mode) do
+    cond do
+      game_mode == 1 -> [%HumanPlayer{mark: "X"}, %HumanPlayer{mark: "O"}]
+      game_mode == 2 -> [%HumanPlayer{mark: "X"}, %RandomComputerPlayer{mark: "O"}]
+      game_mode == 3 -> [%HumanPlayer{mark: "X"}, %UnbeatableComputerPlayer{mark: "O"}]
+    end
+  end
 end
