c06a9103b19bf00747b5bc3582bc44a2f3272115
Himalee <himalee.tailor@gmail.com>
2019-07-04 09:46:30 +0100

    Refactor: update atom names to include _

--- test/game_test.exs
+++ test/game_test.exs
@@ -5,9 +5,9 @@ defmodule GameTest do
 
   setup(_context) do
     {:ok, [
-      humanvhuman: fn ->  Game.new([%HumanPlayer{mark: "X"},  %HumanPlayer{mark: "O"}]) end,
-      randomvrandom: fn -> Game.new([%RandomComputerPlayer{mark: "X", length_of_pause_before_move: 0},  %RandomComputerPlayer{mark: "O", length_of_pause_before_move: 0}]) end,
-      aivsai: fn -> Game.new([%UnbeatableComputerPlayer{mark: "X", length_of_pause_before_move: 0},  %UnbeatableComputerPlayer{mark: "O", length_of_pause_before_move: 0}]) end
+      human_v_human: fn ->  Game.new([%HumanPlayer{mark: "X"},  %HumanPlayer{mark: "O"}]) end,
+      random_v_random: fn -> Game.new([%RandomComputerPlayer{mark: "X", length_of_pause_before_move: 0},  %RandomComputerPlayer{mark: "O", length_of_pause_before_move: 0}]) end,
+      ai_v_ai: fn -> Game.new([%UnbeatableComputerPlayer{mark: "X", length_of_pause_before_move: 0},  %UnbeatableComputerPlayer{mark: "O", length_of_pause_before_move: 0}]) end
     ]}
   end
 
@@ -20,26 +20,26 @@ defmodule GameTest do
   end
 
   test "greets players", context do
-    assert capture_io(moves_for_draw(), context[:humanvhuman]) =~ "Welcome"
+    assert capture_io(moves_for_draw(), context[:human_v_human]) =~ "Welcome"
   end
 
   test "given valid player board size choice, display 3x3 board", context do
-    assert capture_io(moves_for_draw(), context[:humanvhuman]) =~ "  0   1   2 \n  3   4   5 \n  6   7   8 \n"
+    assert capture_io(moves_for_draw(), context[:human_v_human]) =~ "  0   1   2 \n  3   4   5 \n  6   7   8 \n"
   end
 
   test "marks board until full", context do
-    assert capture_io(moves_for_draw(), context[:humanvhuman]) =~ "  X   O   X \n  O   O   X \n  X   X   O \n"
+    assert capture_io(moves_for_draw(), context[:human_v_human]) =~ "  X   O   X \n  O   O   X \n  X   X   O \n"
   end
 
   test "marks board until win", context do
-    assert capture_io(moves_for_win(), context[:humanvhuman]) =~ "  X   X   X \n  3   O   5 \n  O   7   8 \n\nPlayer X wins!\n"
+    assert capture_io(moves_for_win(), context[:human_v_human]) =~ "  X   X   X \n  3   O   5 \n  O   7   8 \n\nPlayer X wins!\n"
   end
 
   test "random computer player vs random computer player plays entire game", context do
-    assert capture_io("3\n", context[:randomvrandom]) =~ "Game over"
+    assert capture_io("3\n", context[:random_v_random]) =~ "Game over"
   end
 
   test "unbeatable computer player vs unbeatable computer player plays entire game and draws", context do
-    assert capture_io("3\n", context[:aivsai]) =~ "draw"
+    assert capture_io("3\n", context[:ai_v_ai]) =~ "draw"
   end
 end
